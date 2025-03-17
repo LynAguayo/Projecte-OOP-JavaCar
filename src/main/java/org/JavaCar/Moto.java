4052eba3a37ee983041a9e0411d1e82b6e57bbfd
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-17 09:21:45 +0100

    Creacio del constructor Moto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -3,4 +3,8 @@ package org.JavaCar;
 public class Moto extends Vehicle{
     private int cilindrada;
 
+    public Moto(String matricula, String marca, String model, double preuBase, int cilindrada, Motor motor, List<Roda> rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes, "C"); // Etiqueta ambiental per defecte
+        this.cilindrada = cilindrada;
+    }
 }
