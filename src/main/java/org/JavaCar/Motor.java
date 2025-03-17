0e0c9e22b73f5219dc5fbffc88e73b47099afb19
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-03-17 09:16:07 +0100

    Constructor motor

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -1,4 +1,14 @@
 package org.JavaCar;
 
 public class Motor {
+    // Variables
+    public String tipus;
+    public double potencia;
+
+    // Constructor per inicialitzar el motor
+    public Motor(String tipus, double potencia) {
+        this.tipus = tipus;
+        this.potencia = potencia;
+    }
+
 }
