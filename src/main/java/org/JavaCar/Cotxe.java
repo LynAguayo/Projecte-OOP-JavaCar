2c8fefa980a57b9a033d8ce7a8ff827b98f19666
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-17 09:37:32 +0100

    Modificacio del constructor Cotxes

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -4,7 +4,7 @@ public class Cotxe extends Vehicle{
     private int nombrePlaces;
 
     public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, List<Roda> rodes) {
-        super(matricula, marca, model, preuBase, motor, rodes, "B");
+        super(matricula, marca, model, preuBase, motor, rodes);
         this.nombrePlaces = nombrePlaces;
     }
 
