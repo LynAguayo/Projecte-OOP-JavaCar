3ac4290af1795c63781a617db727721d503aca57
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-17 09:02:07 +0100

    Creacio del constructor Vehicle

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -8,4 +8,14 @@ public abstract class Vehicle implements Llogable{
     protected Motor motor;
     protected List<Roda> rodes;
     protected String etiquetaAmbiental;
+
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, List<Roda> rodes, String etiquetaAmbiental) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = etiquetaAmbiental;
+    }
 }
