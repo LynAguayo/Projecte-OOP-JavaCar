8036076312eeb0c6cef37c1d19d3484fa339ac06
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-03-17 09:05:17 +0100

    Constructor Roda

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -2,4 +2,14 @@ package org.JavaCar;
 
 public class Roda {
 
+    public String marca;
+    public double diametre;
+
+    // Constructor per inicialitzar la roda
+    public Roda(String marca, double diametre) {
+        this.marca = marca;
+        this.diametre = diametre;
+    }
+
+
 }
