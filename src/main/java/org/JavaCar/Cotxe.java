2f6b0c88cedeebcad84fab263746575c4fd037ae
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-17 09:15:34 +0100

    Creacio del constructor Cotxe

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -3,4 +3,8 @@ package org.JavaCar;
 public class Cotxe extends Vehicle{
     private int nombrePlaces;
 
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, List<Roda> rodes) {
+        super(matricula, marca, model, preuBase, motor, rodes, "B");
+        this.nombrePlaces = nombrePlaces;
+    }
 }
