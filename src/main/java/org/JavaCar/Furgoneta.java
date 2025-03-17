7b6b480ce5d45f1a46994e0e243237613a94b009
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-17 09:38:44 +0100

    Modificacio del constructor Furgoneta

--- src/main/java/org/JavaCar/Furgoneta.java
+++ src/main/java/org/JavaCar/Furgoneta.java
@@ -4,7 +4,7 @@ public class Furgoneta extends Vehicle{
     private double capacitatCarga;
 
     public Furgoneta(String matricula, String marca, String model, double preuBase, double capacitatCarga, Motor motor, List<Roda> rodes) {
-        super(matricula, marca, model, preuBase, motor, rodes, "E"); // Etiqueta ambiental per defecte
+        super(matricula, marca, model, preuBase, motor, rodes);
         this.capacitatCarga = capacitatCarga;
     }
 
