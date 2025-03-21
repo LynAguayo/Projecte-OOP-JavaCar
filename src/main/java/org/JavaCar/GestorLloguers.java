4a8c46d73e50e48c39a941d5206d8599dbf62a0c
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-03-21 12:23:32 +0100

    Afegir comentari funció

--- src/main/java/org/JavaCar/GestorLloguers.java
+++ src/main/java/org/JavaCar/GestorLloguers.java
@@ -1,6 +1,12 @@
 package org.JavaCar;
 
 public class GestorLloguers {
+    /**
+     * Calcula els ingressos totals pel lloguer de tots els vehicles durant un nombre determinat de dies.
+     * @param vehicles Llista de vehicles disponibles per al lloguer
+     * @param dies Nombre de dies de lloguer.
+     * @return El total d'ingressos generat pel lloguer.
+     */
     public static double calcularIngressosTotals(List<Vehicle> vehicles, int dies){
 
     }
