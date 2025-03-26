b60e332a02732ae5bdcf1e794b770f6c36751cce
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-03-26 11:40:35 +0100

    Creació classe Roda

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -1,35 +1,4 @@
 package org.JavaCar;
 
 public class Roda {
-
-    // Variables
-    private String marca;
-    private double diametre;
-
-    // Constructor per inicialitzar la roda
-    public Roda(String marca, double diametre) {
-        this.marca = marca;
-        this.diametre = diametre;
-    }
-
-    // Getter i Setter de la Marca
-    public String getMarca() {
-        return marca;
-    }
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    // Getter i Setter del Diametre
-    public double getDiametre() {
-        return diametre;
-    }
-    public void setDiametre(double diametre) {
-        this.diametre = diametre;
-    }
-
-    @Override
-    public String toString() {
-        return "Roda{ Marca: " + marca + " / Diàmetre: " + diametre+ "}";
-    }
 }
