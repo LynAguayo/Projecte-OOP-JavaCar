cd2923c64485822992464e6f7720f03e57a44d5e
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-26 19:47:28 +0100

    Constructor, setters i getters

--- src/main/java/org/JavaCar/Vehicle.java
+++ src/main/java/org/JavaCar/Vehicle.java
@@ -22,4 +22,49 @@ public abstract class Vehicle implements Llogable{
     private static final int EURO_4_MAX_ANY = 2009;
     private static final int EURO_5_MAX_ANY = 2014;
 
+    // Constructor
+    public Vehicle(String matricula, String marca, String model, double preuBase, Motor motor, Roda[] rodes) {
+        validarMatricula(matricula);
+        this.matricula = matricula;
+        this.marca = marca;
+        this.model = model;
+        this.preuBase = preuBase;
+        this.motor = motor;
+        this.rodes = rodes;
+        this.etiquetaAmbiental = calcularEtiquetaAmbiental();
+    }
+
+    // Setters i getters
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public double getPreuBase() {
+        return preuBase;
+    }
+
+    public Motor getMotor() {
+        return motor;
+    }
+
+    public Roda[] getRodes() {
+        return rodes;
+    }
+
+    public String getEtiquetaAmbiental() {
+        return etiquetaAmbiental;
+    }
+
+    public void setPreuBase(double preuBase) { // s'utilitza a lloguerService
+        this.preuBase = preuBase;
+    }
+
 }
