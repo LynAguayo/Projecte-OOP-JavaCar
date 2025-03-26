7855928bdbe0476e927a26f008b8b2b1cf53d98e
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-03-26 10:10:34 +0100

    Declarar variables

--- src/main/java/org/JavaCar/Motor.java
+++ src/main/java/org/JavaCar/Motor.java
@@ -5,34 +5,5 @@ public class Motor {
     private String tipus;
     private double potencia;
 
-    // Constructor per inicialitzar el motor
-    public Motor(String tipus, double potencia) {
-        this.tipus = tipus;
-        setPotencia(potencia);
-    }
 
-    // Getter i Setter de Tipus
-    public String getTipus() {
-        return tipus;
-    }
-    public void setTipus(String tipus) {
-        this.tipus = tipus;
-    }
-
-    // Getter i Setter de Potencia
-    public double getPotencia() {
-        return potencia;
-    }
-    public void setPotencia(double potencia) {
-        if (potencia > 0) {
-            this.potencia = potencia;
-        } else {
-            throw new IllegalArgumentException("La potència ha de ser un valor positiu!");
-        }
-    }
-
-    @Override
-    public String toString() {
-        return "Motor {Tipus: " + tipus + " / Potencia: " + potencia + "}";
-    }
 }
