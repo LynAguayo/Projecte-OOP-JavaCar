a9cacd94a49e8c2e642bf17d32436488efb12da1
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-26 11:14:27 +0100

    Correció cotxe

--- src/main/java/org/JavaCar/Cotxe.java
+++ src/main/java/org/JavaCar/Cotxe.java
@@ -3,7 +3,7 @@ package org.JavaCar;
 public class Cotxe extends Vehicle{
     private int nombrePlaces;
 
-    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, List<Roda> rodes) {
+    public Cotxe(String matricula, String marca, String model, double preuBase, int nombrePlaces, Motor motor, Roda[] rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
         this.nombrePlaces = nombrePlaces;
     }
