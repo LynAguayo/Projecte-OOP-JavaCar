61a1d829fe0c3133d9f3801710633efe10f3101f
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-26 20:07:06 +0100

    Afegir comentaris a clase Roda

--- src/main/java/org/JavaCar/Roda.java
+++ src/main/java/org/JavaCar/Roda.java
@@ -1,32 +1,32 @@
 package org.JavaCar;
 
 public class Roda {
-
-    // Variables
+    // Atributs
     private String marca;
     private double diametre;
 
-    // Constructor per inicialitzar la roda
+    // Constructor
     public Roda(String marca, double diametre) {
         this.marca = marca;
         this.diametre = diametre;
     }
 
-    // Getter de la Marca
+    // Getters
+
     public String getMarca() {
         return marca;
     }
 
-    // Getter del Diametre
     public double getDiametre() {
         return diametre;
     }
 
+    // Representació en format String
     @Override
     public String toString() {
-
-        return "Roda{ " +
-                "Marca: " + marca + " / Diàmetre: " + diametre+ "}";
+        return "Roda{" +
+                "marca='" + marca + '\'' +
+                ", diametre=" + diametre +
+                '}';
     }
 }
-
