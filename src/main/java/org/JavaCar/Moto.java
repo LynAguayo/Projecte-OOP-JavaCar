f9f2b9b2d97f436d2e8b8f78bf82d5774379013a
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-26 11:21:32 +0100

    Correció roda moto

--- src/main/java/org/JavaCar/Moto.java
+++ src/main/java/org/JavaCar/Moto.java
@@ -3,7 +3,7 @@ package org.JavaCar;
 public class Moto extends Vehicle{
     private int cilindrada;
 
-    public Moto(String matricula, String marca, String model, double preuBase, int cilindrada, Motor motor, List<Roda> rodes) {
+    public Moto(String matricula, String marca, String model, double preuBase, int cilindrada, Motor motor, Roda[] rodes) {
         super(matricula, marca, model, preuBase, motor, rodes);
         this.cilindrada = cilindrada;
     }
