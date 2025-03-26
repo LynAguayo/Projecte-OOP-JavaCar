4db0a09f2524dbd256c99be6e7ced95876799fbc
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2025-03-26 12:50:40 +0100

    Variables

--- src/main/java/org/JavaCar/Client.java
+++ src/main/java/org/JavaCar/Client.java
@@ -1,4 +1,10 @@
 package org.JavaCar;
 
 public class Client {
+    // Variables
+    private String dni;
+    private String nom;
+    private String adreca;
+    private String tipus; // si és "client" o "admin"
+
 }
