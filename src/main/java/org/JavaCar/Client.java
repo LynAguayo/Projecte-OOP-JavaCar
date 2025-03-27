22541e81987b319d03d9a34c54e1172b5d9ab72f
LynAguayo <lyn.aguayoc@gmail.com>
2025-03-27 03:31:06 +0100

    Getters, setters i métodes per gestionar arxius

--- src/main/java/org/JavaCar/Client.java
+++ src/main/java/org/JavaCar/Client.java
@@ -1,3 +1,5 @@
+package org.JavaCar;
+
 public class Client {
     // Atributs
     private String dni;
@@ -17,3 +19,53 @@ public class Client {
         this.adreca = adreca;
         this.punts = punts;
     }
+
+    // Getters
+    public String getDni() {
+        return dni;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getAdreca() {
+        return adreca;
+    }
+
+    public int getPunts() {
+        return punts;
+    }
+
+    // Setters
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setAdreca(String adreca) {
+        this.adreca = adreca;
+    }
+
+    public void setPunts(int punts) {
+        this.punts = punts;
+    }
+
+    // Pasa el client a String per guardar en arxiu
+    public String toFileString() {
+        return dni + ";" + nom + ";" + adreca + ";" + punts;
+    }
+
+    // Crear un client des d'un String de fitxer
+    public static Client fromFileString(String linia) {
+        String[] parts = linia.split(";");
+        if (parts.length != 4) {
+            return null;
+        }
+        try {
+            return new Client(parts[0], parts[1], parts[2], Integer.parseInt(parts[3]));
+        } catch (NumberFormatException e) {
+            System.out.println("Error en format de punts: " + e.getMessage());
+            return null;
+        }
+    }
+}
